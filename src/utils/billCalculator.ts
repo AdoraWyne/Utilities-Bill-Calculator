d7a031d32b77b853d66ea445a5149165add8f637
AdoraWyne <dec_kwynez@hotmail.com>
2026-07-01 12:17:20 +1000

    update billCalculators

--- src/utils/billCalculator.ts
+++ src/utils/billCalculator.ts
@@ -1,15 +1,26 @@
-export const calculateTotalInclusiveDays = (startDate: string, endDate: string): number | null => {
-    if(startDate !== "" && endDate !== ""){
-      // dates inclusive
-      return Temporal.PlainDate.from(startDate).until(endDate).days + 1;
-    }
-    return null
-  };
+import { Temporal } from "temporal-polyfill";
 
-export function calculateBillPerPersonDay(totalBill: number, totalPersonDays: number){
-    return totalBill / totalPersonDays
+export const calculateTotalInclusiveDays = (
+  startDate: string,
+  endDate: string,
+): number | null => {
+  if (startDate !== "" && endDate !== "") {
+    // dates inclusive
+    return Temporal.PlainDate.from(startDate).until(endDate).days + 1;
+  }
+  return null;
+};
+
+export function calculateBillPerPersonDay(
+  totalBill: number,
+  totalPersonDays: number,
+) {
+  return totalBill / totalPersonDays;
 }
 
-export function calculateBillPerDay(billPerPersonDay, daysAtHome){
-    return billPerPersonDay * daysAtHome
+export function calculateBillPerDay(
+  billPerPersonDay: number,
+  daysAtHome: number,
+) {
+  return billPerPersonDay * daysAtHome;
 }
