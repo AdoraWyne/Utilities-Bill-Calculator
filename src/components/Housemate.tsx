4ac6332e3302fba77dc29f1b7fb905360adde384
AdoraWyne <dec_kwynez@hotmail.com>
2026-07-01 13:53:37 +1000

    Add styles to the Travel Dates selection

--- src/components/Housemate.tsx
+++ src/components/Housemate.tsx
@@ -1,3 +1,5 @@
+import styles from "./Housemate.module.css";
+
 type HousemateProps = {
   housemateName: string;
   utilityType?: string;
@@ -24,33 +26,51 @@ const Housemate = ({
   return (
     <div>
       <h3>{housemateName}</h3>
-      <div>
-        <label>
-          Travel from:{" "}
-          <input
-            type="date"
-            name={`${housemateName}-travel-start-date`}
-            value={travelStartDate}
-            onChange={(e) => setTravelStartDate(e.target.value)}
-          />
-          <span>The day start counting absent.</span>
-        </label>{" "}
-        <label>
-          Travel to:{" "}
-          <input
-            type="date"
-            name={`${housemateName}-travel-end-date`}
-            value={travelEndDate}
-            onChange={(e) => setTravelEndDate(e.target.value)}
-          />
-          <span>The last day start counting absent.</span>
-        </label>
+      <div className={styles.travelDateContainer}>
+        <div>
+          <label>
+            Travel from:{" "}
+            <input
+              type="date"
+              name={`${housemateName}-travel-start-date`}
+              value={travelStartDate}
+              onChange={(e) => setTravelStartDate(e.target.value)}
+              aria-describedby={`${housemateName}-travel-start-hint`}
+            />
+          </label>{" "}
+          <p
+            id={`${housemateName}-travel-start-hint`}
+            className={styles.travelHelperText}
+          >
+            The first day starts counting absent.
+          </p>
+        </div>
+        <div>
+          <label>
+            Travel to:{" "}
+            <input
+              type="date"
+              name={`${housemateName}-travel-end-date`}
+              value={travelEndDate}
+              onChange={(e) => setTravelEndDate(e.target.value)}
+              aria-describedby={`${housemateName}-travel-end-hint`}
+            />
+          </label>
+          <p
+            id={`${housemateName}-travel-end-hint`}
+            className={styles.travelHelperText}
+          >
+            The last day starts counting absent.
+          </p>
+        </div>
       </div>
       <p>Total travel days: {totalTravelDays ? totalTravelDays : 0}</p>
       <p>Total home days: {totalHomeDays} </p>
       <p>
         Total {utilityType} bill: ${bill ? bill.toFixed(2) : "-"}
       </p>
+
+      <hr />
     </div>
   );
 };
